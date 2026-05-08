5ebfdfaa7532111916c1f3823ea916d3ce7b7b7d
PAAWSA03LC0313 <PAAWSA03LC0313@PAAWSA03LC0313.duoc.acad>
2026-05-07 20:40:12 -0400

    Clases hoy

---
+++ auth-service/src/main/java/com/smartlogix/auth/domain/Role.java
@@ -0,0 +1,11 @@
+package com.smartlogix.auth.domain;
+
+/**
+ * Roles disponibles en la plataforma SmartLogix.
+ */
+public enum Role {
+    ROLE_USER,
+    ROLE_ADMIN,
+    ROLE_WAREHOUSE_MANAGER,
+
+}
